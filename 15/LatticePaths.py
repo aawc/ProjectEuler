be8396ed92bcf5e778894d7b3d7db8391d2f80b3
Varun Khaneja <khaneja+github@gmail.com>
2014-02-09 03:25:07 -0800

    Adding the solution for the lattice paths problem with explanation and final answer

---
+++ 15/LatticePaths.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# For a lattice path of size n x n, the total number of paths is: C(2n, n)
+# Therefore, for a matrix of size 20 x 20, the answer is C(40, 20), which is:
+print 137846528820
